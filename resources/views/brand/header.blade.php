68d5168acc8b73c45b10b22d3559e04989ce30bf
Mario Kiefer <mario.kiefer@a1.net>
2022-01-21 00:01:48 +0100

    Initial commit

---
+++ resources/views/brand/header.blade.php
@@ -0,0 +1,14 @@
+@push('head')
+    <meta name="robots" content="noindex" />
+    <link
+            href="/favicon.ico"
+            id="favicon"
+            rel="icon"
+    >
+@endpush
+
+<div class="h2 fw-light d-flex align-items-center">
+    <p class="ms-3 my-0 d-none d-sm-block">
+        Scenario Creator
+    </p>
+</div>
