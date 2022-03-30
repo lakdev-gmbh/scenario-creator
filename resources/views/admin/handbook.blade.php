96f17d82b2e9c3e52594550632e94c2c436e30ec
Mario Kiefer <mario.kiefer@a1.net>
2022-03-30 15:21:11 +0200

    Added handbook

--- resources/views/admin/handbook.blade.php
+++ resources/views/admin/handbook.blade.php
@@ -6,15 +6,122 @@
             <h2 class="mt-2 text-dark fw-light">
                 {{ __('Welcome to the Scenario Creator handbook!') }}
             </h2>
-
-{{--            <p>--}}
-{{--                {{ __('') }}--}}
-{{--            </p>--}}
         </div>
         <div class="col col-lg-6 mt-6 p-4 pe-md-0">
-            <img style="max-height: 100px;" class="img-fluid" src="/img/co-financed-EU-de.png"  alt="{{ __('Co-financed by the European Union') }}"/>
+            <img style="max-height: 100px;" class="img-fluid" src="/img/co-financed-EU-de.png"
+                 alt="{{ __('Co-financed by the European Union') }}"/>
         </div>
     </div>
 
+    <style>
+        .section {
+            margin-bottom: 25px;
+        }
+    </style>
+    <div id="wrapper">
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-3">
+                    <nav class="docs-sidebar sticky-top" id="navbar-handbook" data-offset-top="300"
+                         data-offset-bottom="200" role="navigation">
+                        <ul>
+                            <li><a href="#line1">Getting Started</a></li>
+                            <li><a href="#line2">Erstellen eines Szenarios</a></li>
+                            <li><a href="#line3">Erstellen einer Aufgaben-Gruppe</a></li>
+                            <li><a href="#line4">Erstellen von Info-Texten</a></li>
+                            <li><a href="#line5">Erstellen von Aufgaben</a>
+                                <ul>
+                                    <li><a href="#line5_1">Allgemeines</a></li>
+                                    <li><a href="#line5_2">Multiple-Choice Aufgaben</a></li>
+                                    <li><a href="#line5_3">Multiple-Choice Bild Aufgaben</a></li>
+                                    <li><a href="#line5_4">Text Aufgaben</a></li>
+                                    <li><a href="#line5_5">Numerische Aufgaben</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div data-spy="scroll" data-target="#navbar-handbook" data-offset="0" class="col-md-9">
+
+
+                    <section id="line1" class="section">
+                        <h2 class="dark-text">
+                            Getting Started
+                            <hr>
+                        </h2>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p>Lorem the It is a long established fact that a reader will be distracted by the
+                                    readable content of a page when looking at its layout. The point of using Lorem
+                                    Ipsum is that it has a more-or-less normal distribution of letters, as opposed to
+                                    using 'Content here, content here', making it look like readable English.</p>
+                            </div>
+                            <!-- end col -->
+                        </div>
+                        <!-- end row -->
+                    </section>
+                    <!-- end section -->
+
+                    <section id="line2" class="section">
+                        <h2 class="dark-text">
+                            Erstellen eines Szenarios
+                            <hr>
+                        </h2>
+
+
+                        <div class="intro2 clearfix">
+                            <p>
+                                Um ein Szenario zu erstellen, klicken Sie oben auf ...
+                            </p>
+                        </div>
+
+
+                    </section>
+                    <!-- end section -->
+
+                    <section id="line3" class="section">
+                        <h2 class="dark-text">Erstellen einer Aufgaben-Gruppe
+                            <hr>
+                        </h2>
+                        <p>Um eine Aufgaben-Gruppe zu erstellen, klicken Sie auf ...</p>
+                    </section>
+                    <!-- end section -->
+
+                    <section id="line4" class="section">
+                        <h2 class="dark-text">
+                            Erstellen von Info Texten
+                            <hr>
+                        </h2>
+
+                        <p>Um einen Info-Text zu erstellen, klicken Sie auf ...</p>
+                    </section>
+                    <section id="line5" class="section">
+                        <h2 class="dark-text">
+                            Erstellen von Aufgaben
+                            <hr>
+                        </h2>
+
+                        <h3 id="line5_1">Allgemeines</h3>
+                        <p>Aufgaben beinhalten immer einen ...</p>
+                        <h3 id="line5_2">Multiple-Choice Aufgaben</h3>
+                        <p>Multiple Choice Aufgaben ...</p>
+
+                        <p>Um einen Info-Text zu erstellen, klicken Sie auf ...</p>
+                    </section>
+                    <!-- end section -->
+                </div>
+                <!-- // end .col -->
+
+            </div>
+            <!-- // end .row -->
+
+        </div>
+        <!-- // end container -->
+
+    </div>
+    <!-- end wrapper -->
 </div>
 
