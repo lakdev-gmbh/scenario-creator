8f8c140c0c869aa6a41afb0ae8459a2a395fed40
cliebig2019 <82951475+cliebig2019@users.noreply.github.com>
2022-10-04 17:32:44 +0200

    Cors, Hover

--- public/js/admin/quill.hover.js
+++ public/js/admin/quill.hover.js
@@ -1,11 +1,6 @@
-document.addEventListener('orchid:quill', (event) => {
+document.addEventListener("turbo:load", () => {
 
-    //let quill = event.detail.quill;
-    let quill = event.target;
-    //alert(quill);
-    alert(event.target)
-
-    document.ready(function() { alert("loaded") });
+    // based on https://github.com/quilljs/quill/issues/650
 
     let showTooltip = (el) => {
         let tool = el.className.replace('ql-', '');
@@ -14,18 +9,35 @@ document.addEventListener('orchid:quill', (event) => {
             $(el).attr("title", tooltips[tool]);
         }
     };
+
     let tooltips = {
         'bold': 'Bold (ctrl+B)',
         'italic': 'Italic (ctrl+I)',
         'underline': 'Underline (ctrl+U)',
-        'strike': 'Strike'
+        'strike': 'Strike',
+        'link': 'Link',
+        'clean': 'Clean',
+        'color ql-picker ql-color-picker': 'Font color',
+        'background ql-picker ql-color-picker': 'Background color',
+        'blockquote': 'Quote',
+        'code-block': 'Code',
+        'header': 'Header',
+        'list': 'List',
+        'indent': 'Indent',
+        'align ql-picker ql-icon-picker': 'Align',
+        'image': 'Image',
+        'video': 'Video'
     };
 
     let toolbarElement = document.querySelector('.ql-toolbar');
     if (toolbarElement) {
         let matches = toolbarElement.querySelectorAll('button');
-        for (let el of matches) {
-            showTooltip(el);
+        for (let element of matches) {
+            showTooltip(element);
+        }
+        let spans = toolbarElement.querySelectorAll('span');
+        for (let element of spans) {
+            showTooltip(element);
         }
     }
-})
+});
